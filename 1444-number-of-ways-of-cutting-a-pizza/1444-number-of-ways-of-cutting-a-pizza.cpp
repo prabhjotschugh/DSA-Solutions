6d5fcf50d598ec4cf204503f378fa67d4d07416f
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-03-31 20:42:22 +0530

    Time: 23 ms (84.03%), Space: 8.1 MB (64.92%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ways(vector<string>& pizza, int k) {
+        int m = pizza.size(), n = pizza[0].size();
+        vector<vector<vector<int>>> dp(k, vector<vector<int>>(m, vector<int>(n, -1)));
+        vector<vector<int>> preSum(m+1, vector<int>(n+1, 0));
+        for (int r = m - 1; r >= 0; r--)
+            for (int c = n - 1; c >= 0; c--)
+                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r][c] == 'A' ? 1 : 0);
+        return dfs(m, n, k-1, 0, 0, dp, preSum);
+    }
+    int dfs(int m, int n, int k, int r, int c, vector<vector<vector<int>>>& dp, vector<vector<int>>& preSum) {
+        if (preSum[r][c] == 0) return 0;
+        if (k == 0) return 1;
+        if (dp[k][r][c] != -1) return dp[k][r][c];
+        int ans = 0;
+        for (int nr = r + 1; nr < m; nr++) 
+            if (preSum[r][c] - preSum[nr][c] > 0) 
+                ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1000000007;
+        for (int nc = c + 1; nc < n; nc++) 
+            if (preSum[r][c] - preSum[r][nc] > 0)
+                ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1000000007;
+        return dp[k][r][c] = ans;
+    }
+};
