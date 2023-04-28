3a87ec0932ee83233c6ae30247056d4eafe87ac9
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-04-28 14:39:32 +0530

    Time: 163 ms (50.79%), Space: 10.4 MB (52.70%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+     bool similar(string &s,string &t){
+        int dif=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]){
+                dif++;
+            }
+        }
+        return dif<=2;
+    }
+    void dfs(int node,vector<int>&vis,vector<vector<int>>&graph){
+        vis[node]=1;
+        for(auto&child:graph[node]){
+            if(!vis[child]){
+                dfs(child,vis,graph);
+            }
+        }
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        int n=strs.size();
+        vector<vector<int>>graph(strs.size());
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(similar(strs[i],strs[j])){
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+        }
+        int ans=0;
+        vector<int>vis(n);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,graph);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
