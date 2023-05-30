fbf4c785c48f053beceecb6c544c640070e2d6f2
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-05-30 19:28:40 +0530

    Time: 16 ms (44.41%), Space: 15.6 MB (47.16%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,18 +4,16 @@ public:
         int n=nums.size();
         int i=0;
         int j=n-1;
-        for(int i=0;i<nums.size();i++){
-            while(i<=j){
-                 if(nums[i] + nums[j] == target){
-                    return {i+1, j+1};
-                }
-                else if(nums[i] + nums[j] > target){
-                    j--;
-                }
-                else{
-                    i++;
-                }  
+        while(i<=j){
+             if(nums[i] + nums[j] == target){
+                return {i+1, j+1};
             }
+            else if(nums[i] + nums[j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }  
         }
         return vector<int> ();
     }
