5749870642fa3aa3f0a8ab83e546c471e7eaa7f0
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-05-31 21:27:15 +0530

    Added solution - LeetHub

---
+++ Count Squares - GFG/count-squares.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        float num = sqrt(N);
+        int ans = ceil(num)-1;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
