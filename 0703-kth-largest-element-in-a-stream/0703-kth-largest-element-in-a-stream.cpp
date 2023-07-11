b984e153ac989d130b80f8ac96f979b5386c0696
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-07-11 19:48:04 +0530

    Time: 45 ms (22.82%), Space: 19.7 MB (71.72%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -5,27 +5,16 @@ public:
     KthLargest(int k, vector<int>& nums) {
         size = k;
         for(auto i:nums){
-            if(pq.size()<k){
-                pq.push(i);
-                continue;
-            }
-            if(i>pq.top()){
-                pq.pop();
-                pq.push(i);
-            }
+            add(i);
         }
     }
     
     int add(int val) {
-        if(pq.size()<size){
-                pq.push(val);
-                return pq.top();
-            }
-            if(val>pq.top()){
-                pq.pop();
-                pq.push(val);
-            }
-            return pq.top();
+        pq.push(val);
+        if(pq.size()>size){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
 
