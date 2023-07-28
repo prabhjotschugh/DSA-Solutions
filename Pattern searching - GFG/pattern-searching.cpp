3c3142687f5d0e98b3cab4d61088d00fd415088f
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-07-28 21:46:32 +0530

    Added solution - LeetHub

---
+++ Pattern searching - GFG/pattern-searching.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+str: string in which pattern we have to find pattern.
+text: pattern needs to searched.
+*/
+
+bool searchPattern(string str, string pat)
+{
+    // your code here
+    return (str.find(pat)!=string::npos);
+}
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    
+    while(t--)
+    {
+        string str, pat;
+        cin>>str;
+        cin>>pat;
+        
+        if(searchPattern(str, pat) == true)
+            cout << "Present" <<endl;
+        else cout << "Not present" <<endl;
+    }
+}
+
+
+// } Driver Code Ends
