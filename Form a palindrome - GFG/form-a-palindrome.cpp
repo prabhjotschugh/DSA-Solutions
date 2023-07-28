da62cabf1bdf1b41b5c45b42763fc013d966b959
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-07-28 21:51:05 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int lcs(int n, int m, string s1, string s2)
+    {
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int countMin(string a){
+    //complete the function here
+        string b = a;
+        reverse(b.begin(), b.end());
+        int n = a.length();
+        int lps = lcs(n, n, a, b);
+        return n-lps;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
