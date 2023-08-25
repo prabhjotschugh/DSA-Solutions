c0fe107a3f1fb20f66b226f80c4ffabe60d33933
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-08-25 19:58:13 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+    
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    if(head1 == NULL)
+        return head2;
+        
+    if(head2 == NULL)
+        return head1;
+        
+    if(head1->data <= head2->data){
+        head1->bottom = sortedMerge(head1->bottom, head2);
+        return head1;
+    }else{
+        head2->bottom = sortedMerge(head1, head2->bottom);
+        return head2;
+    }
+}  
+
+Node *flatten(Node *root)
+{
+    if(root == NULL || root->next == NULL)
+        return root;
+        
+    root->next = flatten(root->next);
+    
+    root = sortedMerge(root, root->next);
+    return root;
+}
+
