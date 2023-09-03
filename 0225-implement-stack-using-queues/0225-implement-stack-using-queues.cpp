7c4d72e56dace4403f9b2dc3efd2db2be0548312
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-09-03 15:19:10 +0530

    Time: 3 ms (38.87%), Space: 6.8 MB (65.12%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,32 +1,28 @@
-#include <queue>
-
 class MyStack {
-private:
-    std::queue<int> q;
-
 public:
+    queue<int> q;
     MyStack() {}
-
+    
     void push(int x) {
         q.push(x);
-        for (int i = 0; i < q.size() - 1; ++i) {
+        for(int i=0;i<q.size()-1;i++){
             q.push(q.front());
             q.pop();
         }
     }
-
+    
     int pop() {
-        int val = q.front();
+        int ans = q.front();
         q.pop();
-        return val;
+        return ans;
     }
-
+    
     int top() {
         return q.front();
     }
-
+    
     bool empty() {
-        return q.empty();
+        return q.size()==0;
     }
 };
 
