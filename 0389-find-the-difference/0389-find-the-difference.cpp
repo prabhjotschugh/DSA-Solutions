94cc07571b92610faecc3b5e96b3da238b75904f
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-09-25 11:21:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> v(26, 0);
+        for(auto& c:s) 
+            v[c-'a']+=1;
+        for(auto& c:t) 
+            if(--v[c-'a']<0) 
+                return c;
+        
+        return '\0';
+    }
+};
