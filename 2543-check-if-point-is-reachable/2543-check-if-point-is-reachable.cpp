63e6c2db22ed13c6db15e0b127683d67bb7fc240
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-10-11 19:56:38 +0530

    Time: 2 ms (55.74%), Space: 6.3 MB (21.72%) - LeetHub

---
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isReachable(int targetX, int targetY) {
+        int g = __gcd(targetX, targetY);
+        while(g % 2 == 0) 
+            g /= 2;
+        
+        return g == 1;
+    }
+};
