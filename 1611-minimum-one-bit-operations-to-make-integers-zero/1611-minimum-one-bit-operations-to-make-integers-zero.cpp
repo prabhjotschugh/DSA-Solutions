66a11ae089688930f16e34f34974a3dad16a51be
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-11-30 20:43:34 +0530

    Time: 3 ms (45.97%), Space: 6.4 MB (32.70%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if (n==0) 
+            return 0; 
+        
+        int k=floor(log2(n));  
+        int numOp=(1<<(k+1))-1;
+        return numOp-minimumOneBitOperations(n^(1<<k));
+    }
+};
