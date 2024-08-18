aff4e99f78dfbbfa19d32fcdc348c5efb4cee07b
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2024-08-18 21:05:25 +0530

    Time: 6 ms (55.94%), Space: 9.7 MB (56.48%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> t(n + 1);
+        t[1] = 1; 
+        int t2 = 1;
+        int t3 = 1;
+        int t5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int second = t[t2] * 2;
+            int third = t[t3] * 3;
+            int fifth = t[t5] * 5;
+            t[i] = min({second, third, fifth});
+            if (t[i] == second) {
+                t2++;
+            }
+            if (t[i] == third) {
+                t3++;
+            }
+            if (t[i] == fifth) {
+                t5++;
+            }
+        }
+        return t[n];
+    }
+};
