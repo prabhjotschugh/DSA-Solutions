4321ad771a802d2474af7da37fd0d5fcbc976aab
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2024-08-22 11:49:21 +0530

    Time: 3 ms (41.07%), Space: 7.5 MB (57.13%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1; 
+        unsigned int bitmask = ~0; 
+        while (num & bitmask) {
+            bitmask <<= 1;
+        }
+        return num ^ ~bitmask;
+    }
+};
