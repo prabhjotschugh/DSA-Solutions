3294f49f6630479f00a476c2606d8af06b112e23
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2024-09-17 12:41:26 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (28.22%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    map<string,int>mp;
+    
+    void stringstreeam(string s){
+        stringstream ss(s);
+        string word;
+        while(ss>>word) mp[word]++;
+    }
+
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        stringstreeam(s1);
+        stringstreeam(s2);
+
+        vector<string>ans;
+        for(auto x:mp)
+            if(x.second==1) 
+                ans.push_back(x.first);
+
+        return ans;
+    }
+};
