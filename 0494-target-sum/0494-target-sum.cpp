1609f7f81fe0ebb1c78acba900614c77c2219505
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2024-12-26 17:15:56 +0530

    Time: 2 ms (90.56%), Space: 15.9 MB (38.17%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int subsetSum(vector<int>& arr, int n, int sum){
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        dp[0][0] = 1;
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j - arr[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+    
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int n=arr.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+        }
+        
+        if((sum + target)%2 != 0){
+            return 0;
+        }
+        int targetSum = (sum + abs(target))/2;
+        return subsetSum(arr, n, targetSum);
+    }
+};
