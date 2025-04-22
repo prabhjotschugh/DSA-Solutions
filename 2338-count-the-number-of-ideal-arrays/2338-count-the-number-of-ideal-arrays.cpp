7b2989c80994119578c181e83f35240fffff0c7b
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2025-04-22 18:19:58 +0530

    Time: 3 ms (91.74%), Space: 9 MB (75.23%) - LeetHub

---
+++ 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.cpp
@@ -0,0 +1,29 @@
+int comb[10001][14] = { 1 }, cnt[10001][14] = {}, mod = 1000000007;
+
+class Solution {
+public: 
+    int idealArrays(int n, int maxValue) {
+        if (comb[1][1] == 0) { // One-time precomputation
+            // Precompute binomial coefficients: comb[s][r] = C(s, r)
+            for (int s = 1; s <= 10000; ++s)
+                for (int r = 0; r < 14; ++r)
+                    comb[s][r] = r == 0 ? 1 : (comb[s - 1][r - 1] + comb[s - 1][r]) % mod;
+
+            // Modified Sieve: cnt[i][bars] = # of sequences ending in i with (bars+1) terms
+            for (int div = 1; div <= 10000; ++div) {
+                ++cnt[div][0];
+                for (int i = 2 * div; i <= 10000; i += div)
+                    for (int bars = 0; cnt[div][bars]; ++bars)
+                        cnt[i][bars + 1] += cnt[div][bars];
+            }
+        }
+
+        int res = 0;
+        // Count valid arrays using precomputed tables
+        for (int i = 1; i <= maxValue; ++i)
+            for (int bars = 0; bars < min(14, n) && cnt[i][bars]; ++bars)
+                res = (1LL * cnt[i][bars] * comb[n - 1][bars] + res) % mod;
+
+        return res;
+    }
+};
