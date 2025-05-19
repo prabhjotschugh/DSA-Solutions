c8f41a984b353bc1c0a10c158c74f990642df6a8
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2025-05-19 14:39:23 +0530

    Time: 0 ms (100%), Space: 23.1 MB (8.96%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        set<int>s(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
+        if((nums[0] + nums[1])<=nums[2]) return "none";
+        if(s.size() == 1) return "equilateral";
+        if(s.size() == 2) return "isosceles";
+        return "scalene";
+    }
+};
