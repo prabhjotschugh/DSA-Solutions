9e2600d3c2cfd3414b83794a635a63a9ec7307b0
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2025-05-26 17:58:05 +0530

    Time: 232 ms (99.86%), Space: 119.1 MB (100%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,40 +1,44 @@
+const int N=100000;
+vector<int> adj[N];
+int q[N], front=0, back=0;
+int freq[N][26];
 class Solution {
 public:
-    int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.size(),k=26;
-        vector<int> indegrees(n, 0);
-        vector<vector<int>> graph(n, vector<int>());
-        for (auto edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            indegrees[v]++;
+    static int largestPathValue(string colors, vector<vector<int>>& edges) {
+        const int n=colors.size();
+        for(int i=0; i<n; i++)
+            adj[i].clear();
+        vector<int> deg(n, 0);
+        for (auto& e : edges) {
+            const int u=e[0], v=e[1];
+            adj[u].push_back(v);
+            deg[v]++;
         }
-        unordered_set<int> zero_indegree;
-        for (int i = 0; i < n; i++) {
-            if (indegrees[i] == 0) {
-                zero_indegree.insert(i);
+    
+        back=front=0;
+        for(int i=0; i<n; i++){
+            memset(freq[i], 0, 26*sizeof(int));
+            if (deg[i]==0){
+                q[back++]=i;
+                freq[i][colors[i]-'a']++;
             }
         }
-        vector<vector<int>> counts(n, vector<int>(k, 0));
-        for (int i = 0; i < n; i++) {
-            counts[i][colors[i] - 'a']++;
-        }
-        int max_count = 0,visited = 0;
-        while (!zero_indegree.empty()) {
-            int u = *zero_indegree.begin();
-            zero_indegree.erase(u);
+        int ans=0, visited=0;
+        while(front!=back){
+            int u=q[front++]; 
+            int x=*max_element(freq[u], freq[u]+26);
+            ans=max(ans, x);
             visited++;
-            for (int v : graph[u]) {
-                for (int i = 0; i < k; i++)
-                    counts[v][i] = max(counts[v][i], counts[u][i] + (colors[v] - 'a' == i ? 1 : 0));
-                indegrees[v]--;
-                if (indegrees[v] == 0) {
-                    zero_indegree.insert(v);
+            for(int v:adj[u]){
+                for(int i=0; i<26; i++){
+                    int uv=freq[u][i]+(i==colors[v]-'a');
+                    freq[v][i]=max(freq[v][i], uv );
                 }
+                if (--deg[v]==0) q[back++]=v;
             }
-            max_count = max(max_count, *max_element(counts[u].begin(), counts[u].end()));
+
+
         }
-        return visited == n ? max_count : -1;
+        return visited<n ? -1 : ans;
     }
 };
