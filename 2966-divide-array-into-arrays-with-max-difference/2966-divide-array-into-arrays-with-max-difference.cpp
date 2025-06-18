63a9da2c422ef38461b3c01b73dd01caefabee22
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2025-06-18 21:37:52 +0530

    Time: 35 ms (96.41%), Space: 111.3 MB (88.52%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        for(int i = 2; i < nums.size(); i += 3){
-            if(nums[i] - nums[i - 2] > k) return {};
-            res.push_back({nums[i-2], nums[i-1], nums[i]});
+    static vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        int freq[100001]={0};
+        int xMax=0;
+        for(int x: nums){
+            freq[x]++;
+            xMax=max(x, xMax);
         }
-        return res;
+        vector<vector<int>> ans(n/3);
+        vector<int> vec3(3);
+        int count=0, vec_m, i=0;
+        for(int x=1; x<=xMax; x++){
+            for( ; freq[x]>0; freq[x]--, count++){
+                if (count%3==0)
+                    vec_m=x;
+                if (x>vec_m+k ) return {};
+                vec3[count]=x;
+                if (count%3==2){
+                    ans[i++]=vec3;
+                    count=-1;
+                }
+            }
+        }
+        return ans;
     }
 };
